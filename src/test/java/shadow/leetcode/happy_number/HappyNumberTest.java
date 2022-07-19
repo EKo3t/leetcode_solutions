f5f8e8aebf182e4e21959e62343ace698e70b57f
Shadow <ekostukevich@gmail.com>
2022-07-19 11:01:26 +0400

    happy number

---
+++ src/test/java/shadow/leetcode/happy_number/HappyNumberTest.java
@@ -0,0 +1,25 @@
+package shadow.leetcode.happy_number;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class HappyNumberTest {
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    public void shouldReturnCorrectResults(int given, boolean answer) {
+        var objectUnderTest = new HappyNumber();
+        Assertions.assertEquals(answer, objectUnderTest.isHappy(given));
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+            Arguments.of(19, true),
+            Arguments.of(2, false)
+        );
+    }
+}
