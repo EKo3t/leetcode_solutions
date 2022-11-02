50b27a6b278ee6874ac108b5f81c77e75d7d58b6
Shadow <ekostukevich@gmail.com>
2022-11-02 12:57:38 +0400

    Maximum difference between increasing elements

---
+++ src/main/java/shadow/leetcode/MaximumDifferenceBetweenIncreasingElements.java
@@ -0,0 +1,22 @@
+package shadow.leetcode;
+
+public class MaximumDifferenceBetweenIncreasingElements {
+
+    public int maximumDifference(int[] nums) {
+        int[] maximums = new int[nums.length];
+        int max = Integer.MIN_VALUE;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (max < nums[i])
+                max = nums[i];
+
+            maximums[i] = max;
+        }
+        int maxDiff = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (maxDiff < maximums[i + 1] - nums[i]) {
+                maxDiff = maximums[i + 1] - nums[i];
+            }
+        }
+        return maxDiff == 0 ? -1 : maxDiff;
+    }
+}
